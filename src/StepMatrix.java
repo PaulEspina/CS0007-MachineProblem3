51a5a305765d47b8d74a5049a187d074511b8fbe
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-06-14 01:57:36 +0800

    added rows and cols label data

--- src/StepMatrix.java
+++ src/StepMatrix.java
@@ -1,17 +1,25 @@
 public class StepMatrix
 {
     private int[][] matrix; // the reduced matrix at this step
+    private int[] rowLabel;
+    private int[] colLabel;
     private int[] rowMin; // the minimum of each row before reducing the matrix
     private int[] colMin; // the minimum of each col before reducing the matrix
     private int nodeCount; // the remaining nodes at this step
     private int removedRow; // the removed row at this step
     private int removedCol; // the removed col at this step
 
-    public StepMatrix(int[][] matrix, int removedRow, int removedCol, int[]rowMin, int[] colMin)
+    public StepMatrix(int[][] matrix, int removedRow, int removedCol, int[]rowMin, int[] colMin, int[]rowLabel, int[]colLabel)
     {
-        this.matrix = matrix;
+        this.matrix = new int[matrix.length][matrix.length];
+        for(int i = 0; i < matrix.length; i++)
+        {
+            this.matrix[i] = matrix[i].clone();
+        }
         this.removedRow = removedRow;
         this.removedCol = removedCol;
+        this.rowLabel = rowLabel;
+        this.colLabel = colLabel;
         nodeCount = matrix.length;
     }
 
@@ -45,5 +53,15 @@ public class StepMatrix
     public int[] getColMin()
     {
         return colMin;
+    }
+
+    public int[] getRowLabel()
+    {
+        return rowLabel;
+    }
+
+    public int[] getColLabel()
+    {
+        return colLabel;
     }
 }
